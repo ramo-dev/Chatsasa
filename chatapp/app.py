ab4c13aecc56c392014d124ce9b072edc6c21492
ramo-dev <136629650+ramo-dev@users.noreply.github.com>
2023-12-12 12:11:48 +0300

    Update app.py

--- chatapp/app.py
+++ chatapp/app.py
@@ -16,4 +16,4 @@ def handle_message(data):
     emit('message', data, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True, port=5000, host='0.0.0.0', allow_unsafe_werkzeug=True)
